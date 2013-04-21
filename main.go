cbbd122d37c11007d227b529e0ad23a5a7a750ad
Markus Pieton <marpie@a12d404.net>
2013-04-21 21:45:11 +0200

    Code execution added.

--- main.go
+++ main.go
@@ -8,8 +8,23 @@ import (
 	"flag"
 	"fmt"
 	"github.com/marpie/winfsnotify"
+	"os"
+	"os/exec"
 	"strings"
 )
+
+func parseArguments(parameters []string, event *winfsnotify.Event) []string {
+	out_params := make([]string, len(parameters))
+	i := 0
+	for _, entry := range parameters {
+		output := strings.Replace(entry, "[[Cookie]]", fmt.Sprintf("%d", event.Cookie), -1)
+		output = strings.Replace(output, "[[Filename]]", event.Name, -1)
+		output = strings.Replace(output, "[[Info]]", event.String(), -1)
+		out_params[i] = output
+		i += 1
+	}
+	return out_params
+}
 
 func main() {
 	w, err := winfsnotify.NewWatcher()
@@ -20,23 +35,20 @@ func main() {
 
 	event := flag.String("event", "all", "possible Events: all, access, close, create, delete, modify, move.")
 	one_shot := flag.Bool("one-shot", false, "only execute once (true/false)")
-	format := flag.String("format", "[[Filename]]", "[[Cookie]]=unique cookie of the event, [[Name]]=File/Directory name, [[Info]]=Debug information.")
 	//recursive := flag.Bool("recursive", true, "only fire on directory events  (true/false)")
 	flag.Parse()
 
 	command := flag.Arg(0)
+	parameters := flag.Args()[1:]
 
 	if len(command) < 1 {
-		println("Command to execute is missing!")
+		fmt.Printf("%s [command] [param1 [param2 [param3]...]])\n\n", os.Args[0])
+		flag.Usage()
+		println("\nThe supplied command is parsed for the values, that are replace with the received event values:")
+		println("  [[Cookie]]=unique cookie of the event\n  [[Name]]=File/Directory name\n  [[Info]]=Debug information.")
+		println("\nExamples:\nOpen newly created files/folders in Explorer:\n  notifyexec.exe -event=create explorer.exe [[Filename]]")
 		return
 	}
-
-	/*
-		output-format:
-			[[Cookie]]
-			[[Filename]]
-			[[Info]]
-	*/
 
 	var mode uint32 = winfsnotify.FS_ALL_EVENTS // listen to all events
 	switch *event {
@@ -63,23 +75,21 @@ func main() {
 	}
 
 	if err := w.AddWatch(".", mode); err != nil {
-		fmt.Printf("Error adding watch: %v", err)
+		fmt.Printf("Error adding watch: %v\n", err)
 		return
 	}
 
 	for event := range w.Event {
-		output := strings.Replace(*format, "[[Cookie]]", fmt.Sprintf("%d", event.Cookie), -1)
-		output = strings.Replace(output, "[[Filename]]", event.Name, -1)
-		output = strings.Replace(output, "[[Info]]", event.String(), -1)
+		println(event.String())
+		output := parseArguments(parameters, event)
 
-		// DEBUG
-		println(output)
-		// DEBUG END
+		cmd := exec.Command(command, output...)
+		if err := cmd.Start(); err != nil {
+			fmt.Printf("Error while running command: %v\n", err)
+		}
 
 		if *one_shot {
 			break
 		}
 	}
-
-	println("Done.")
 }
