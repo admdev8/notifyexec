82a2dd5e1ec626c4c4eed155d2b177dcec4955ea
Markus Pieton <marpie@a12d404.net>
2013-04-22 18:10:45 +0200

    regex filtering added.

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ import (
 	"github.com/marpie/winfsnotify"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
+	"time"
 )
 
 func parseArguments(parameters []string, event *winfsnotify.Event) []string {
@@ -35,19 +37,39 @@ func main() {
 
 	event := flag.String("event", "all", "possible Events: all, access, close, create, delete, modify, move.")
 	one_shot := flag.Bool("one-shot", false, "only execute once (true/false)")
-	//recursive := flag.Bool("recursive", true, "only fire on directory events  (true/false)")
+	pace := flag.Bool("pace", true, "only execute once a second. (true/false)")
+	filterRegex := flag.String("filter", ".*", "filter by regex. (default: include all)")
+	excludeRegex := flag.String("exclude", "", "exclude by regex. (default: exclude none)")
+	//recursive := flag.Bool("recursive", true, "recursively wait for events. (true/false)")
 	flag.Parse()
 
 	command := flag.Arg(0)
-	parameters := flag.Args()[1:]
-
 	if len(command) < 1 {
 		fmt.Printf("%s [command] [param1 [param2 [param3]...]])\n\n", os.Args[0])
 		flag.Usage()
 		fmt.Printf("\nThe supplied command is parsed for the values, that are replace with the received event values:")
 		fmt.Printf("  [[Cookie]]=unique cookie of the event\n  [[Name]]=File/Directory name\n  [[Info]]=Debug information.")
-		fmt.Printf("\nExamples:\nOpen newly created files/folders in Explorer:\n  notifyexec.exe -event=create explorer.exe [[Filename]]")
+		fmt.Printf("\nExamples:\nOpen newly created files/folders in Explorer:\n  notifyexec.exe -event=create explorer.exe [[Filename]]\n")
 		return
+	}
+	parameters := flag.Args()[1:]
+
+	// set-up regex
+	var filter, exclude *regexp.Regexp
+	if *filterRegex != "" {
+		filter, err = regexp.Compile(*filterRegex)
+		if err != nil {
+			fmt.Printf("Error compiling regex: %v", err)
+			return
+		}
+	}
+
+	if *excludeRegex != "" {
+		exclude, err = regexp.Compile(*excludeRegex)
+		if err != nil {
+			fmt.Printf("Error compiling regex: %v", err)
+			return
+		}
 	}
 
 	var mode uint32 = winfsnotify.FS_ALL_EVENTS // listen to all events
@@ -79,11 +101,36 @@ func main() {
 		return
 	}
 
+	last := time.Now().Unix()
 	for event := range w.Event {
+		if *pace {
+			now := time.Now().Unix()
+			if last == now {
+				time.Sleep(1 * time.Second)
+				last = now
+				continue
+			}
+			last = now
+		}
+
+		if *excludeRegex != "" {
+			if exclude.MatchString(event.Name) {
+				continue
+			}
+		}
+
+		if *filterRegex != "" {
+			if !filter.MatchString(event.Name) {
+				continue
+			}
+		}
 		println(event.String())
+
 		output := parseArguments(parameters, event)
 
 		cmd := exec.Command(command, output...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 		if err := cmd.Start(); err != nil {
 			fmt.Printf("Error while running command: %v\n", err)
 		}
